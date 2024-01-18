9bddc7d01162f6061f48f147128093b1fef00631
JiJetu <jijetu2@gmail.com>
2024-01-18 11:38:10 +0600

    created room card

--- src/Componenets/Home/HomeTestimonial.jsx
+++ src/Componenets/Home/HomeTestimonial.jsx
@@ -3,7 +3,17 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const HomeTestimonial = () => {
-    
+    const [reviews, setReviews] = useState([]);
+
+    console.log(reviews);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/reviews')
+            .then((res) => res.json())
+            .then((data) => {
+                setReviews(data);
+            });
+    }, []);
 
     return (
         <div className="bg-white p-4">
